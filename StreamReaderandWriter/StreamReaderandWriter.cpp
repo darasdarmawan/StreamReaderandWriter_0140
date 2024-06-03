1c4a824f343b160b5e0e53c69b1dc84b2da8682e
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-06-03 09:51:57 +0700

    memasukan kondisi while true

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -6,7 +6,20 @@ using namespace std;
 int main() {
 	string baris;
 
+	//membuka file dalam mode menulis
 	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q'\ untuk keluar" << endl;
+
+	//unlimited loop untuk mmenulis
+	while (true) {
+		cout << "- ";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
+	}
 
 
 }
